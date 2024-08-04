872ab96ef3d44fa6ee3957a8925c04336e4e1abd
InnaTymoshenko <innatymosh@gmail.com>
2024-08-04 19:14:30 +0300

    created navTabs component

--- components/nav-tabs.tsx
+++ components/nav-tabs.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import React from 'react'
+import { usePathname, useSelectedLayoutSegment } from 'next/navigation'
 import Link from 'next/link'
 import { VariantProps, cva } from 'class-variance-authority'
 import { cn } from '@/utils/cn'
@@ -6,13 +9,11 @@ import { cn } from '@/utils/cn'
 type Props = {}
 
 export const tabStyles = cva(
-	[
-		'gap-2 inline-flex items-center justify-center whitespace-nowrap px-3 py-3 text-secondary font-medium hover:text-primary disabled:pointer-events-none disabled:opacity-50 border-b-2 border-transparent'
-	],
+	['w-[32px] h-[32px] border border-white rounded-full flex justify-center items-center text-white font-semibold'],
 	{
 		variants: {
 			selected: {
-				true: 'border-b-2 border-selected text-primary'
+				true: 'text-blue-300 bg-blue-50'
 			}
 		}
 	}
@@ -21,8 +22,10 @@ export const tabStyles = cva(
 export interface TabProps extends React.ComponentPropsWithoutRef<typeof Link>, VariantProps<typeof tabStyles> {}
 
 const NavTab = React.forwardRef<React.ElementRef<typeof Link>, TabProps>(
-	({ className, href, selected, ...props }, ref) => (
-		<Link ref={ref} href={href} className={cn(tabStyles({ selected }), className)} {...props} />
+	({ className, href, selected, children, ...props }, ref) => (
+		<Link ref={ref} href={href} className={cn(tabStyles({ selected }), className)} {...props}>
+			{children}
+		</Link>
 	)
 )
 NavTab.displayName = 'NavTab'
@@ -32,20 +35,25 @@ interface NavTabsProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const NavTabs = ({ className, children, items, ...props }: NavTabsProps) => {
-	// const segment = useSelectedLayoutSegment()
-	// const pathname = usePathname()
+	const segment = useSelectedLayoutSegment()
+	const pathname = usePathname()
 
 	return (
-		<div className={cn('flex items-center border-b', className)} {...props}>
+		<div className={cn('flex flex-col gap-8 pt-8', className)} {...props}>
 			{items.map((item, index) => {
 				return (
-					<NavTab
-						key={index}
-						href={item.href}
-						// selected={segment === null ? item.href === pathname : item.href.includes(String(segment))}
-					>
-						{item.title}
-					</NavTab>
+					<div key={index} className="flex gap-4 px-8 items-center">
+						<NavTab
+							href={item.href}
+							selected={segment === null ? item.href === pathname : item.href.includes(String(segment))}
+						>
+							{index + 1}
+						</NavTab>
+						<div className="flex flex-col justify-between items-start tracking-widest">
+							<span className="text-sm text-blue-100">{`STEP ${index + 1}`}</span>
+							<strong className="text-lg text-white">{item.title}</strong>
+						</div>
+					</div>
 				)
 			})}
 		</div>
